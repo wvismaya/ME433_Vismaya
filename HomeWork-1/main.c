51dc8b52376118c23c9bbd6cac05830302ac5ea5
wvismaya <vismayawalawalkar2018@u.northwestern.edu>
2017-04-11 09:22:10 -0500

    Update in main code

--- HomeWork-1/main.c
+++ HomeWork-1/main.c
@@ -1,44 +1,47 @@
-// Yay! Part one done till Setting up IDE. Now for blinky!
-
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
 
+// PIC32MX250F128B Configuration Bit Settings
+
+// 'C' source line config statements
 // DEVCFG0
-#pragma config DEBUG = x // no debugging
-#pragma config JTAGEN = ON // no jtag
+#pragma config DEBUG = 0b11 // no debugging
+#pragma config JTAGEN = OFF // no jtag
 #pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
 #pragma config PWP = OFF // no write protect
 #pragma config BWP = OFF // no boot write protect
 #pragma config CP = OFF // no code protect
 
 // DEVCFG1
-#pragma config FNOSC = FRCDIV // use primary oscillator with pll
-#pragma config FSOSCEN = ON // turn off secondary oscillator
-#pragma config IESO = ON // no switching clocks
-#pragma config POSCMOD = OFF // high speed crystal mode
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
 #pragma config OSCIOFNC = OFF // disable secondary osc
-#pragma config FPBDIV = DIV_8 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
 #pragma config FCKSM = CSDCMD // do not enable clock switch
 #pragma config WDTPS = PS1048576 // use slowest wdt
 #pragma config WINDIS = OFF // wdt no window mode
-#pragma config FWDTEN = ON // wdt disabled
+#pragma config FWDTEN = OFF // wdt disabled
 #pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
-#pragma config FPLLIDIV = DIV_12 // divide input clock to be in range 4-5MHz
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
 #pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
-#pragma config FPLLODIV = DIV_256 // divide clock after FPLLMUL to get 48MHz
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
 #pragma config UPLLIDIV = DIV_12 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
-#pragma config UPLLEN = OFF // USB clock on
+#pragma config UPLLEN = ON // USB clock on
 
 // DEVCFG3
-#pragma config USERID = 0 // some 16bit userid, doesn't matter what
-#pragma config PMDL1WAY = ON // allow multiple reconfigurations
-#pragma config IOL1WAY = ON // allow multiple reconfigurations
+#pragma config USERID = 2017 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
 #pragma config FUSBIDIO = ON // USB pins controlled by USB module
 #pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
 
-#define DELAY_TIME 1000
+void delay(void);
 
 int main() {
 
@@ -57,25 +60,38 @@ int main() {
     DDPCONbits.JTAGEN = 0;
 
     // do your TRIS and LAT commands here
-    TRISA = 0x0000;
-    TRISB = 0xFFFF;
 
     __builtin_enable_interrupts();
-    int i = 0;
-
+    
+    TRISBbits.TRISB4=1; //making B4 into PORT for usrbutton
+    TRISBbits.TRISB8=1; //making B4 into PORT for usrbutton
+    TRISBbits.TRISB7=0; //making B7 into LAT for output
+    TRISAbits.TRISA4=0; //making A4 into LAT for LED output
+    LATBbits.LATB7=1;
+    LATAbits.LATA4=1;
+    
     while(1) {
+//        LATBbits.LATB7=!PORTBbits.RB4;
+        _CP0_SET_COUNT(0);
+        while(_CP0_GET_COUNT()<25000){
+            ;
+            while(!PORTBbits.RB4) {
+                  ;   // Pin B4 is the USER switch, low (FALSE) if pressed.
+             }    
+        }        
+        LATAINV=0b10000;
         // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
           // remember the core timer runs at half the sysclk
-        //LATA = 0x0000;
-        while(!PORTBbits.RB7){
-            LATAbits.LATA1 = 0xFFFF;
-            _CP0_SET_COUNT(0);
-            while(_CP0_GET_COUNT() <= DELAY_TIME);
-            //for(i=0; i<=1000; i++);
-            LATAbits.LATA1 = 0x0000;
-            _CP0_SET_COUNT(0);
-            while(_CP0_GET_COUNT() <= DELAY_TIME);
-            //for(i=0; i<=1000; i++);   
-        }
     }
+    return 0;
+}
+
+void delay(void) {
+  int j;
+  for (j = 0; j < 1000000; j++) { // number is 1 million
+      ;
+    while(!PORTBbits.RB4) {
+        ;   // Pin B4 is the USER switch, low (FALSE) if pressed.
+    }
+  }
 }
